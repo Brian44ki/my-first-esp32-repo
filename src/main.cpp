0dc8b4d304d01b36d4717fd3a812d9743e6239ed
brian kipruto <chepchiengkipruto@gmail.com>
2026-06-24 14:53:41 +0300

    i have but variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include <Arduino.h>
-
+int redledpin=8;
 int ledpin=2;
 int delaytime=700;
 
 void setup() {
   pinMode(ledpin,OUTPUT);
-  
+  pinMode(redledpin,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(ledpin,HIGH);
-  delay(delaytime);
-  digitalWrite(2,LOW);
-  delay(delaytime);
-
-
-
+digitalWrite(ledpin,HIGH);
+delay(delaytime);
+digitalWrite(ledpin,LOW);
+delay(delaytime);
+digitalWrite(redledpin,HIGH);
+delay(delaytime);
+digitalWrite(redledpin,LOW);
+delay(delaytime);
 }
-
-
